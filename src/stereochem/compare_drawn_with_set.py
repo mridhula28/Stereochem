cb52d94950004ee87df2139374263ea833365786
frenot <lucie.frenot@epfl.ch>
2025-04-26 19:23:28 +0200

    updtate before switching to lucie

--- src/stereochem/compare_drawn_with_set.py
+++ src/stereochem/compare_drawn_with_set.py
@@ -3,18 +3,23 @@
 import streamlit as st
 from rdkit import Chem 
 from rdkit.Chem import Draw
+from streamlit_ketcher import st_ketcher
 from stereochem.generate_isomers import generate_isomers
 from stereochem.example_module import example_module
 
-if smile_code:  # Only proceed if user has drawn something
-    drawn_mol = Chem.MolFromSmiles(smile_code)
-    
-    if drawn_mol:
-        # Canonical SMILES with stereochemistry
-        drawn_canon_smiles = Chem.MolToSmiles(drawn_mol, isomericSmiles=True, canonical=True)
+# compare the smile of the drawn molecule with the simles of the set of stereoisomers
 
-        # Display drawn structure
-        st.image(Draw.MolToImage(drawn_mol), caption="Drawn Molecule")
+smile_code = st_ketcher(molecule_name)
+
+if smile_code:                                              # It only proceeds if user has drawn something
+    st.session_state["drawn_smiles"] = smile_code
+
+if "drawn_smiles" in st.session_state:
+    drawn_mol = Chem.MolFromSmiles(st.session_state["drawn_smiles"])
+    
+    if drawn_mol:                                                           # Canonical SMILES with stereochemistry
+        drawn_canon_smiles = Chem.MolToSmiles(drawn_mol, isomericSmiles=True, canonical=True)
+        st.image(Draw.MolToImage(drawn_mol), caption="Drawn Molecule")      # Display drawn structure
         st.markdown(f"**Drawn SMILES:** `{drawn_canon_smiles}`")
 
         # Compare with generated stereoisomers
