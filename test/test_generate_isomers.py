deee15011ebee62d50af691f8d3b1da75704f9b1
CatherinaCarrer <catherina.carrer@epfl.ch>
2025-05-08 14:12:57 +0200

    import changed in test

--- test/test_generate_isomers.py
+++ test/test_generate_isomers.py
@@ -1,13 +1,7 @@
 # test/manual_test_generate_isomers.py
 
-import sys
-import os
-
-# Add the 'src' folder to the Python path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
-
 # Import the function from stereochem in src
-from stereochem.generate_isomers import generate_isomers
+from src.stereochem.generate_isomers import generate_isomers
 
 def test_generate_isomers():
     smiles = "CC(O)C(=O)O"  # Example: lactic acid
